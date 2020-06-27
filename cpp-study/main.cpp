833c49b6a101230b5c71dad03ec0d56eb6abbfc0
LT <liutao272@gmail.com>
2020-06-27 20:21:45 +0800

    Initial Commit

---
+++ cpp-study/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp-study
+//
+//  Created by Liu Tao on 2020/6/27.
+//  Copyright © 2020 Liu Tao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
